49b5c7f674b12487bfb86c3c8b2a2f9834f27d55
UmerAliKhan <aumeralikhan@gmail.com>
2024-11-11 00:46:06 +0500

    initial commit

--- index.js
+++ index.js
@@ -3,7 +3,10 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.json({
+    status: 200,
+    message: "Hello World!",
+  });
 });
 
 app.listen(port, () => {
